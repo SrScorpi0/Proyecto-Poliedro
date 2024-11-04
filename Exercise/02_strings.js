2b4ef2027c7e7d14bd8f56a3a6ff1b5511e458d7
Axel Rion <axelrion168@gmail.com>
2024-11-03 23:11:38 -0300

    Ejercicio 2

--- Exercise/02_strings.js
+++ Exercise/02_strings.js
@@ -44,7 +44,7 @@ Guardar el resultado en una nueva variable (utilizar substring, toUpperCase,
 toLowerCase y el operador +).
  */
 
-var frase2 = "Un budin de banana";
+var frase2 = "un budin de banana";
 
 var frase2Modificada = frase2.substring(0,1).toUpperCase() + frase2.substring(1).toLowerCase();
 
@@ -55,3 +55,28 @@ e. Crear una variable de tipo string con al menos 10 caracteres y algún espacio
 en blanco. Encontrar la posición del primer espacio en blanco y guardarla en
 una variable (utilizar indexOf).
  */
+
+var frase3 = "Sacar la pizza del horno";
+
+console.log(frase3);
+
+var posicionPrimerEspacio = frase3.indexOf(" ");
+
+console.log(posicionPrimerEspacio);
+
+/**
+f. Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres
+y algún espacio entre medio). Utilizar los métodos de los ejercicios anteriores
+para generar un nuevo string que tenga la primera letra de ambas palabras en
+mayúscula y las demás letras en minúscula (utilizar indexOf, substring,
+toUpperCase, toLowerCase y el operador +).
+ */
+
+let frase4 = "escribiendo codigo";
+
+console.log(frase4.indexOf("co"));
+
+let frase4Modificada = frase4.substring(0,1).toUpperCase() + frase4.substring(1,11).toLowerCase() + " " + frase4.substring(12,13).toUpperCase() + frase4.substring(13,18).toLowerCase(); 
+
+console.log(frase4Modificada);
+
