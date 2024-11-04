50953b515162d619c5fa0c1f76c0b1e0e8101ef5
Axel Rion <axelrion168@gmail.com>
2024-11-03 21:41:48 -0300

    Ejercicio 1 Variables y Operadores

---
+++ Exercise/01-variables-and-operators.js
@@ -0,0 +1,32 @@
+/**Ejercicio 1
+a) Crear dos variables numéricas y utilizar el operador suma para guardar el valor
+de la suma de ambos números en una 3er variable.
+ */
+
+var a = 5;
+var b = 25;
+
+var c = a + b
+
+console.log(c);
+
+/** b. Crear dos variables de tipo String y concatenarlas guardando el resultado en
+una 3er variable. */
+
+var nombre = "Axel";
+var apellido = "Rion";
+
+var nombreCompleto = nombre + " " + apellido;
+
+console.log(nombreCompleto);
+
+/** c. Crear dos variables de tipo String y sumar el largo de cada variable (cantidad
+de letras del string) guardando el resultado de la suma en una 3er variable
+(utilizar length). */
+
+var palabra1 = "casa";
+var palabra2 = "perro";
+
+var cantidadDeLetras = palabra1.length + palabra2.length
+
+console.log(cantidadDeLetras);
